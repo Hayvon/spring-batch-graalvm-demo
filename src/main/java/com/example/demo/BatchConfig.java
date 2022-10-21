de7e9aedb525e0f804f48c96c44e16659ac9be10
Aleksandr Arshavskiy <aleksandr.arshavskiy@c24.de>
2022-10-21 15:32:47 +0200

    running native image

--- src/main/java/com/example/demo/BatchConfig.java
+++ src/main/java/com/example/demo/BatchConfig.java
@@ -1,9 +1,20 @@
 package com.example.demo;
 
+import java.sql.Types;
+import java.util.Objects;
 import javax.sql.DataSource;
 
+import com.example.demo.BatchConfig.BatchRuntimeHints;
+import org.springframework.aop.SpringProxy;
+import org.springframework.aop.framework.Advised;
+import org.springframework.aot.hint.ExecutableMode;
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.aot.hint.TypeReference;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.aot.CoreRuntimeHints;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
 import org.springframework.batch.core.job.builder.JobBuilder;
@@ -18,10 +29,14 @@ import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportRuntimeHints;
+import org.springframework.core.DecoratingProxy;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.util.ReflectionUtils;
 
 @Configuration
+//@ImportRuntimeHints(BatchRuntimeHints.class)
 public class BatchConfig {
 
     @Bean
@@ -65,5 +80,41 @@ public class BatchConfig {
                 .sql("INSERT INTO people (name, surname) VALUES (:name, :surname)")
                 .dataSource(dataSource)
                 .build();
+    }
+
+    static class BatchRuntimeHints implements RuntimeHintsRegistrar {
+
+        @Override
+        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+            /*hints.resources().registerPattern("org/springframework/batch/core/schema-h2.sql");
+            hints.resources().registerPattern("org/springframework/batch/core/schema-derby.sql");
+            hints.resources().registerPattern("org/springframework/batch/core/schema-hsqldb.sql");
+            hints.resources().registerPattern("org/springframework/batch/core/schema-sqlite.sql");
+            hints.resources().registerPattern("org/springframework/batch/core/schema-db2.sql");
+            hints.resources().registerPattern("org/springframework/batch/core/schema-hana.sql");
+            hints.resources().registerPattern("org/springframework/batch/core/schema-mysql.sql");
+            hints.resources().registerPattern("org/springframework/batch/core/schema-oracle.sql");
+            hints.resources().registerPattern("org/springframework/batch/core/schema-postgresql.sql");
+            hints.resources().registerPattern("org/springframework/batch/core/schema-sqlserver.sql");
+            hints.resources().registerPattern("org/springframework/batch/core/schema-sybase.sql");
+
+            hints.proxies()
+                .registerJdkProxy(builder -> builder
+                    .proxiedInterfaces(TypeReference.of("org.springframework.batch.core.repository.JobRepository"))
+                    .proxiedInterfaces(SpringProxy.class, Advised.class, DecoratingProxy.class))
+                .registerJdkProxy(builder -> builder
+                    .proxiedInterfaces(TypeReference.of("org.springframework.batch.core.explore.JobExplorer"))
+                    .proxiedInterfaces(SpringProxy.class, Advised.class, DecoratingProxy.class));
+            hints
+                .reflection()
+                .registerType(Types.class, MemberCategory.DECLARED_FIELDS);
+            //.registerConstructor(Person.class.getDeclaredConstructor(), ExecutableMode.INVOKE)
+            //.registerMethod(ReflectionUtils.findMethod(Person.class, "setName", String.class), ExecutableMode.INVOKE)
+            //.registerMethod(ReflectionUtils.findMethod(Person.class, "getName"), ExecutableMode.INVOKE)
+            //.registerMethod(ReflectionUtils.findMethod(Person.class, "setSurname", String.class), ExecutableMode.INVOKE)
+            //.registerMethod(ReflectionUtils.findMethod(Person.class, "getSurname"), ExecutableMode.INVOKE);
+
+            //hints.resources().registerPattern("sample-data.csv");*/
+        }
     }
 }
