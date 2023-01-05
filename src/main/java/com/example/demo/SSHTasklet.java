cc71dff336260e2a0e35bdf1506005347023d90d
Aleksandr Arshavskiy <aleksandr.arshavskiy@c24.de>
2023-01-05 11:25:20 +0100

    update version, add more data to sample

--- src/main/java/com/example/demo/SSHTasklet.java
+++ src/main/java/com/example/demo/SSHTasklet.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import net.schmizz.sshj.DefaultConfig;
 import net.schmizz.sshj.SSHClient;
 import net.schmizz.sshj.connection.channel.direct.Session;
 import net.schmizz.sshj.transport.verification.PromiscuousVerifier;
@@ -12,10 +13,13 @@ public class SSHTasklet implements Tasklet {
   @Override
   public RepeatStatus execute(final StepContribution contribution, final ChunkContext chunkContext) throws Exception {
 
-    SSHClient ssh = new SSHClient();
+    DefaultConfig config = new DefaultConfig();
+    config.setBouncycastleRuntimeRegistrationEnabled(false);
+    SSHClient ssh = new SSHClient(config);
+
     ssh.addHostKeyVerifier(new PromiscuousVerifier());
     ssh.connect("localhost", 22022);
-    
+
     ssh.authPassword("root", "mypassword");
     Session session = ssh.startSession();
     session.exec("mkdir /home/test");
--- src/main/java/com/example/demo/SpringBatchGraalvmDemoApplication.java
+++ src/main/java/com/example/demo/SpringBatchGraalvmDemoApplication.java
@@ -1,13 +1,16 @@
 package com.example.demo;
 
+import org.springframework.batch.core.aot.CoreRuntimeHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
+@ImportRuntimeHints(CoreRuntimeHints.class)
 public class SpringBatchGraalvmDemoApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SpringBatchGraalvmDemoApplication.class, args);
   }
-
+  
 }
