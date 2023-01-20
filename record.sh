5d6cb2fd914be3b4d92b7889eca3bedcd2dcf6ba
Aleksandr Arshavskiy <aleksandr.arshavskiy@c24.de>
2023-01-20 16:09:25 +0100

    measurements

--- record.sh
+++ record.sh
@@ -2,5 +2,5 @@ export PATH=$PATH:~/.local/bin
 
 for i in {1..10}
 do
-psrecord "java -jar build/libs/demo-0.0.1-SNAPSHOT.jar" --include-children --interval 1 --duration 10 --plot "jvm_$i.png"
+psrecord "java -jar build/libs/demo-0.0.1-SNAPSHOT.jar" --include-children --interval 0.1 --duration 10 --log "jvm_$i.txt"
 done
