7c52e424493a7a7fd722a308b9bfbc32e0d5b447
Aleksandr Arshavskiy <aleksandr.arshavskiy@c24.de>
2023-03-10 22:44:25 +0100

    clean up

--- measure_cpu_jvm.sh
+++
@@ -1,2 +0,0 @@
-DEMO_COMMAND="java -jar build/libs/demo-0.0.1-SNAPSHOT.jar"
-perf stat $DEMO_COMMAND
--- record_native.sh
+++
@@ -1,2 +0,0 @@
-export PATH=$PATH:~/.local/bin
-psrecord "build/native/nativeCompile/demo" --include-children --interval 1 --duration 900 --plot  native_1mio.png
